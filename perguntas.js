9445f47a209599cad227e034a5cc2274f03a543a
pedrohenrique1329 <pedro.menger.silva@escola.pr.gov.br>
2025-07-01 10:58:14 +0000

    Correção de erros.

---
+++ perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Filme',
+    'recomendação de filme de anime bom que poucos conhecem.',
+    'Redline(2009)'
+)
+criaCartao(
+    'Filme',
+    'recomendação de filme de anime bom que poucos conhecem.',
+    'O serviço de entregas da Kiki'
+)
+criaCartao(
+    'VideoGame',
+    'Recomendação de jogo muito bom que poucos conhecem.',
+    'Inscryption'
+)
+criaCartao(
+    'VideoGame',
+    'Recomendação de jogo muito bom que poucos conhecem.',
+    'Knack'
+)
+criaCartao(
+    'Livro',
+    'Recomendação de livro muito bom.',
+    'Meu pé de laranja lima'
+)
+criaCartao(
+    'Livro',
+    'Recomendação de livro muito bom.',
+    '1984'
+)
+criaCartao(
+    'Livro',
+    'Recomendação de livro muito bom.',
+    'A revolução dos bichos'
+)
+criaCartao(
+    'Filme',
+    'Recomendação de filme muito bom que poucos conhecem.',
+    'Os escritores da liberdade'
+)
+criaCartao(
+    'Filme',
+    'Recomendação de filme muito bom que poucos conhecem.',
+    'Forrest Gump: o contador de histórias'
+)
+criaCartao(
+    'VideoGame',
+    'Recomendação de jogo muito bom que poucos conhecem.',
+    'Overcooked'
+)
+
